ed93871f181058916f93ac34058c1f2c36c90440
sylvain66 <syllv66@outlook.com>
2016-11-27 14:39:01 +0100

    exo fait

---
+++ tableauexo2.php
@@ -0,0 +1,4 @@
+<?php
+    $mois = array('Janvier','Fevrier','Mars','Avril','Mai','juin','juillet','Aout','septembre','octobre','novembre','Decembre');
+      echo  $mois[2];
+ ?>
---
+++ tableauexo4.php
@@ -0,0 +1,5 @@
+
+<?php
+    $mois = array('Janvier','Fevrier','Mars','Avril','Mai','juin','juillet','Aout','septembre','octobre','novembre','Decembre');
+      echo $mois[7] = ('Août');
+ ?>
